b4ca12578f1a0d370716e222b029cf197c4ae376
Dari <cula.lou@hotmail.de>
2021-04-25 12:40:47 +0200

    project created

---
+++ RoutineReminder/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using RoutineReminder.ViewModels;
+
+namespace RoutineReminder.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
